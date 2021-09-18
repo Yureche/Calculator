e664a4a99060d7358c63cc7fe5e7c20c8fb3cae7
Yureche <yurigerber.g18@gmail.com>
2021-09-18 19:16:42 +0300

    Display button value on click

---
+++ app.js
@@ -0,0 +1,62 @@
+// ! Define Variabels
+const currentOperation = document.querySelector(".current-operation");
+
+// * Buttons
+const button0 = document.querySelector("#button0");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const button7 = document.querySelector("#button7");
+const button8 = document.querySelector("#button8");
+const button9 = document.querySelector("#button9");
+
+// Functions
+
+// ! Event Listeners
+// * Numbers
+button0.addEventListener("click",() => {
+    currentOperation.textContent += 0;
+});
+
+button1.addEventListener("click",() => {
+    currentOperation.textContent += 1;
+});
+
+button2.addEventListener("click",() => {
+    currentOperation.textContent += 2;
+});
+
+button3.addEventListener("click",() => {
+    currentOperation.textContent += 3;
+});
+
+button4.addEventListener("click",() => {
+    currentOperation.textContent += 4;
+});
+
+button5.addEventListener("click",() => {
+    currentOperation.textContent += 5;
+});
+
+button6.addEventListener("click",() => {
+    currentOperation.textContent += 6;
+});
+
+button7.addEventListener("click",() => {
+    currentOperation.textContent += 7;
+});
+
+button8.addEventListener('click', () => {
+    currentOperation.textContent += 8;
+});
+
+button9.addEventListener("click",() => {
+    currentOperation.textContent += 9;
+});
+
+// * Operators
+
+
